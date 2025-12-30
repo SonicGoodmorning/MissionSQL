d40436553889f8c46d3a62afa9872cf57d597ef0
Ege <egegoodmorning@gmail.com>
2025-12-30 12:15:17 -0500

    Added Correlation function to see the link between price and ratings.

--- trends.py
+++ trends.py
@@ -13,14 +13,35 @@ def data():
     HAVING game_count > 5 
     ORDER BY price ASC
     '''
+
     ## excluding ftp games due to alot of them not being popular enough causing
     ## skewed results.
-    ##
+    ## gamecount > 5 is just to make this more simple and eliminate games
+    ## that are inflated in price to avoid skewing the results.
+
     df = pd.read_sql_query(query, connection)
+    ## Goes through each data point, and queries.
     connection.close()
     return df
+
+def correlation(df):
+
+    correlation = df['price'].corr(df['avg_reviews'])
+    ## calculate the correlation between the avg review and price
+    ## basically finding out if higher price means better reviews.
+
+    if correlation > .5:
+        ic("Higher Priced games are rated higher.")
+    elif correlation > .1:
+        ic("Higher Prices help game ratings slightly")
+    else:
+        ic("No correlation found between price and quality of game")
+    ic(correlation)
+    
 
 df = data() # establish data frame through func
 ic("Data Ready") 
 
 ic(df.head()) # print out the head of df for simplicity.
+
+correlation(df)
